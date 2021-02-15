5833a382f5415c35cd9a9a8db29bff15e3644895
fanifadil <fanifadilah75@gmail.com>
2021-02-15 19:17:40 +0700

    footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Hero from "src/parts/Hero";
 import Clients from "src/parts/Clients";
 import ListCourses from "src/parts/ListCourses";
 import ListCategories from "src/parts/ListCategories";
+import Footer from "src/parts/Footer";
 
 function Home({ data }) {
   return (
@@ -33,6 +34,9 @@ function Home({ data }) {
         </section>
         <section className='container px-4 mx-auto md:pt-24'>
           <ListCategories></ListCategories>
+        </section>
+        <section className='mt-24 bg-indigo-1000 py-12'>
+          <Footer></Footer>
         </section>
       </main>
     </>
